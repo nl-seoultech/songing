9a0dcc7383b59d9c9097f8a5e58c94f1ad9f0f8a
kyungsun <mysinta@naver.com>
2014-05-10 07:26:05 +0000

    set.py 추가

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+]
+
+test_requires = [
+        'pytest >= 2.3.5'
+]
+
+setup(name='songing',
+      description=u'좋아하는 노래를 분석하는 프로그램',
+      author='Bongyong Choi, Kyungsun Park',
+      author_email='exe91@naver.com, asinta@naver.com',
+      packages=find_packages(),
+      install_requires=install_requires,
+      test_require=test_requires)
